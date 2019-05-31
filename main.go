7387cc682aa484d39e07ad1eef64c892a190ae60
nickmurr <32221055+NickMurr@users.noreply.github.com>
2019-05-31 10:49:47 +0300

    22 - Joining a Slice of Strings

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	cards := newDeck()
 
-	hand, remaindingCards := deal(cards, 5)
+	// hand, remaindingCards := deal(cards, 5)
 
-	hand.print()
-	remaindingCards.print()
+	// hand.print()
+	// remaindingCards.print()
 
+	var test = cards.toString()
+	fmt.Println([]byte(test))
 }
